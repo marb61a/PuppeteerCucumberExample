4c478fa03025d0702438ded713de9750e1572497
Martin <marb61a@gmail.com>
2020-09-02 20:00:37 +0100

    Added login feature and steps file

---
+++ features/support/steps.js
@@ -0,0 +1,17 @@
+const { Given, When, Then, Before, After } = require("cucumber");
+
+Before(async function(){
+    return await this.launchBrowser();
+});
+
+After(async function(){
+    return await this.closeBrowser();
+});
+
+Given("I open login page", async function(){
+    return await this.visit();
+});
+
+When("I fill login form", async function(){
+    return await this.fillLoginForm();
+});
