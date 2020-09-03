b670321a153ac66b8cf49c6aafbfd05146d8007f
Martin <marb61a@gmail.com>
2020-09-03 19:33:09 +0100

    Updated report file and generated report

--- report.js
+++ report.js
@@ -1,3 +1,4 @@
+// Use the npm run generate:report option at the cli
 const reporter = require("cucumber-html-reporter");
 
 const options = {
@@ -7,6 +8,15 @@ const options = {
     // 2) Hierarchy
     // 3) Foundation
     // 4) Simple
-    theme: ""
-    
+    theme: "bootstrap",
+    // Grab the auto created json file
+    jsonFile: "cucumber_report.json",
+    // This will transform the JSON file into html
+    output: "cucumber_report.html",
+    reportSuiteAsScenarios: true,
+    // Auto launch the report
+    launchReport: true
 };
+
+// Pass the options into the report generator
+reporter.generate(options);
